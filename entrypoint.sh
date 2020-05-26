d0577b831a210237ab1348fde9c7d5704c6e5cc4
Simon Jagoe <simon.jagoe@smartly.io>
2020-05-26 12:24:59 +0300

    Put ssh private key in $HOME, outside of the workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+# Directory outside docker container corresponding to $HOME here
+EXT_HOME=/home/runner/work/_temp/_github_home
+
 # Validate input
 
 if [ -z "$GIT_DEPLOY_KEY" ]
@@ -11,14 +14,14 @@ then
 fi
 
 # Setup SSH keys so we can push commits and tags to master branch
-mkdir -p "$GITHUB_WORKSPACE/.ssh"
-ssh-keyscan -t rsa github.com > "$GITHUB_WORKSPACE/.ssh/known_hosts"
-echo "$GIT_DEPLOY_KEY" > "$GITHUB_WORKSPACE/.ssh/id_rsa"
-chmod 400 "$GITHUB_WORKSPACE/.ssh/id_rsa"
+mkdir -p "$HOME/.ssh"
+ssh-keyscan -t rsa github.com > "$HOME/.ssh/known_hosts"
+echo "$GIT_DEPLOY_KEY" > "$HOME/.ssh/id_rsa"
+chmod 400 "$HOME/.ssh/id_rsa"
 
 # Setup git
 git config user.email "$INPUT_EMAIL"
 git config user.name "$INPUT_USERNAME"
-git config core.sshCommand 'ssh -i $GITHUB_WORKSPACE/.ssh/id_rsa -o UserKnownHostsFile=$GITHUB_WORKSPACE/.ssh/known_hosts'
+git config core.sshCommand "ssh -i ${EXT_HOME}/.ssh/id_rsa -o UserKnownHostsFile=${EXT_HOME}/.ssh/known_hosts"
 
 git remote set-url origin "git@github.com:$GITHUB_REPOSITORY.git"
