5752423e8921e54b47dbfe727dd5b603f0774fb4
Simon Jagoe <simon.jagoe@smartly.io>
2021-09-15 10:26:29 +0300

    Fix code for new typescript

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function run(): Promise<void> {
       await cleanupGitAction()
     }
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed((error as Error).message)
   }
 }
 
